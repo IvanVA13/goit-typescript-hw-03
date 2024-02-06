b9db04e2123b22bb1d14f1af2ac41890ed7cd6e0
Ivan Volobuev <71542875+IvanVA13@users.noreply.github.com>
2024-02-06 20:30:13 +0200

    completed hw 3

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,58 @@
+interface IKey {
+  getSignature(): number;
+}
+
+class Key implements IKey {
+  private signature: number;
+  constructor() {
+    this.signature = Math.round(Math.random() * 10000000);
+  }
+
+  getSignature(): number {
+    return this.signature;
+  }
+}
+
+interface IPerson {
+  getKey(): Key;
+}
+
+class Person implements IPerson {
+  constructor(private key: Key) {
+    this.key = key;
+  }
+  getKey(): Key {
+    return key;
+  }
+}
+
+abstract class House {
+  protected door: boolean;
+  protected tenants: IPerson[] = [];
+  constructor(protected key: Key) {
+    this.key = key;
+    this.door = false;
+  }
+
+  comeIn(person: Person): void {
+    if (this.door) {
+      this.tenants.push(person);
+    }
+  }
+  abstract openDoor(key: Key): void;
+}
+
+class MyHouse extends House {
+  constructor(key: Key) {
+    super(key);
+  }
+  openDoor(key: Key): void {
+    if (key.getSignature() === this.key.getSignature()) {
+      this.door = true;
+    }
+  }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
@@ -6,6 +61,5 @@ const person = new Person(key);
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
